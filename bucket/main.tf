62d575f72386b21dd3abde0de5d251ca809f6718
Segey Gorkov <sergey@gorkov.pro>
2023-01-28 20:44:06 +0300

    cloud infrastructure

---
+++ bucket/main.tf
@@ -0,0 +1,47 @@
+terraform {
+    required_providers {
+    yandex = {
+        source = "yandex-cloud/yandex"
+    }
+    }
+    required_version = ">= 0.13"
+}
+
+provider "yandex" {
+    folder_id = "${var.yc_folder_id}"
+    zone = "ru-central1-a"
+}
+
+resource "yandex_iam_service_account" "sa" {
+    name = "sa-bucket"
+}
+
+// Назначение роли сервисному аккаунту
+resource "yandex_resourcemanager_folder_iam_member" "sa-editor" {
+    folder_id = "${var.yc_folder_id}"
+    role      = "storage.editor"
+    member    = "serviceAccount:${yandex_iam_service_account.sa.id}"
+}
+
+// Создание статического ключа доступа
+resource "yandex_iam_service_account_static_access_key" "sa-static-key" {
+    service_account_id = yandex_iam_service_account.sa.id
+    description        = "static access key for object storage"
+}
+
+// Создание бакета с использованием ключа
+resource "yandex_storage_bucket" "test" {
+    access_key = yandex_iam_service_account_static_access_key.sa-static-key.access_key
+    secret_key = yandex_iam_service_account_static_access_key.sa-static-key.secret_key
+    bucket     = "terraformnetologygorkov"
+}
+
+# resource "local_file" "access_key" {
+#     content = yandex_iam_service_account_static_access_key.sa-static-key.access_key
+#     filename = "access_key"
+# }
+
+# resource "local_file" "secret_key" {
+#     content = yandex_iam_service_account_static_access_key.sa-static-key.secret_key
+#     filename = "secret_key"
+# }
--- terraform/main.tf
+++ terraform/main.tf
@@ -11,8 +11,8 @@ terraform {
     bucket     = "terraformnetologygorkov"
     region     = "ru-central1"
     key        = "main/terraform.tfstate"
-    access_key = ""
-    secret_key = ""
+    access_key = "YCAJEwMLaCxtkRAGEKIFgrO3H"
+    secret_key = "YCP2leH5PnydF8xM--4oRul2ma1ViYV0klQrc6ea"
 
     skip_region_validation      = true
     skip_credentials_validation = true
@@ -24,8 +24,3 @@ provider "yandex" {
   zone = "ru-central1-a"
 }
 
-# // Создание статического ключа доступа
-# resource "yandex_iam_service_account_static_access_key" "sa-static-key" {
-#   service_account_id = yandex_iam_service_account.sa.id
-#   description        = "static access key for object storage"
-# }
